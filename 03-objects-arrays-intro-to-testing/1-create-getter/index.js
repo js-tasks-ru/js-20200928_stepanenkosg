701cabb71446bba25ef274773c184e10a92e45ba
stepanenkostas87 <stepanenkostas87@yandex.ru>
2020-10-07 11:03:08 +0300

    Реализована функция создания getter метода.

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,12 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return function (obj) {
+    let result = obj;
+    for (const key of path.split('.')) {
+      if (!Object.keys(result).includes(key)) return undefined;
+      result = result[key];
+    }
+    return result;
+  }
 }
