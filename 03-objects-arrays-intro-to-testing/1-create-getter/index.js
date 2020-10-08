2c9e84018550e023b77713dd8d938ad00cc329fa
stepanenkostas87 <stepanenkostas87@yandex.ru>
2020-10-08 10:02:16 +0300

    Доработал код функции согласно замечаниям.

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,10 +4,11 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const keys = path.split('.');
   return function (obj) {
     let result = obj;
-    for (const key of path.split('.')) {
-      if (!Object.keys(result).includes(key)) return undefined;
+    for (const key of keys) {
+      if (!Object.keys(result).includes(key)) return;
       result = result[key];
     }
     return result;
